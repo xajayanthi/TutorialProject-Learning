6f9c8e112a6e862641b6fb7b00e9a21cae1574ea
xearts <xearts@xeartsnoMac-mini.local>
2019-01-28 12:38:48 +0900

    Updated name of a counter variable

--- PHP/print_prime.php
+++ PHP/print_prime.php
@@ -5,13 +5,13 @@ function displayPrimeNo()
     while (1==1)
     {
         $iNbrToCheckPrime = $iNbrToCheckPrime + 1;
-        $isDividableCnt = 0;
+        $iDividableCnt = 0;
         for ($iDivisorNo = 1; $iDivisorNo <= $iNbrToCheckPrime; $iDivisorNo++) {
             if ($iNbrToCheckPrime % $iDivisorNo == 0) {
-                $isDividableCnt++;
+                $iDividableCnt++;
             }
         }
-        if ($isDividableCnt == 2) {
+        if ($iDividableCnt == 2) {
             echo $iNbrToCheckPrime." is Prime number \n";
         }
     }
