d26ca35bc2f874d4bc1d9e3b100d126f5d8497c2
xearts <xearts@xeartsnoMac-mini.local>
2019-01-30 11:34:33 +0900

    Updated the logic with sqrt function for better performance

--- PHP/print_prime.php
+++ PHP/print_prime.php
@@ -3,22 +3,33 @@ function displayPrimeNo()
 {
     /** @var  $numberToCheck  Number to be checked whether Prime or not*/
     $numberToCheck = 0;
+    echo "The PrimeNumbers are \n";
     while (true)
     {
-        $numberToCheck ++;
-        $isDividable = false;
-        for ($divisorNumber = 2; $divisorNumber < $numberToCheck; $divisorNumber++) {
-            if ($numberToCheck % $divisorNumber == 0) {
-                $isDividable = true;
-                break;
+        $numberToCheck = $numberToCheck+1;
+        /** Even numbers are not prime;
+        Check only the odd numbers whether it is prime*/
+        if ($numberToCheck ==1 or $numberToCheck == 2) {
+            $isDividable = false;
+        }
+        if ($numberToCheck % 2 != 0) {
+            $isDividable = false;
+            $sqrtNumber  = floor(sqrt($numberToCheck));
+            for ($divisorNumber = 2; $divisorNumber <= $sqrtNumber; $divisorNumber+=1) {
+                if ($numberToCheck % $divisorNumber == 0) {
+                    $isDividable = true;
+                    break;
+                }
             }
+            /** Printing Prime numbers */
+            if (!$isDividable ) {
+                echo $numberToCheck."\n";
+            }
+
         }
-        if (!$isDividable ) {
-            echo $numberToCheck." is Prime number \n";
-        }
+
     }
 }
-
 displayPrimeNo();
 
 ?>
