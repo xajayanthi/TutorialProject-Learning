60aee168845cbbc4fb8736f793b962589b90831c
xajayanthi <j.velusamy@xearts.jp>
2019-10-07 15:13:59 +0900

    Form Test

--- symphony/blog/tests/Controller/BlogentriesControllerTest.php
+++ symphony/blog/tests/Controller/BlogentriesControllerTest.php
@@ -25,31 +25,6 @@ Class BlogentriesControllerTest extends WebTestCase
 
         $this->assertResponseIsSuccessful();
     }
-
-    public function testHeaderData(){
-
-        $client = static::createClient();
-
-        $client->request('GET', '/blogentries/');
-
-        $this->assertSelectorTextContains('html h1', 'Welcome to our Blog');
-
-        $this->assertSelectorTextContains('html h2', 'Blogentries index');
-    }
-
-    public function testShow(){
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/blogentries/');
-
-//        $link = $crawler->selectLink("show")->link();
-        $link = $crawler->filter('a:contains("show")')->eq(0)->link();
-
-        $crawler = $client->click($link);
-
-        $this->assertEquals(3, $crawler->filter('th')->count());
-    }
-
     /**
      * @dataProvider provideUrls
      */
@@ -70,12 +45,46 @@ Class BlogentriesControllerTest extends WebTestCase
         ];
     }
 
-//   public function testEditForm(){
-//
-//       $client = static::createClient();
-//
-//       $form=$crawler = $client->request('GET', '/blogentries/new')->selectButton("Save")->form();
-//
-//       $crawler = $client->submit($form);
-//   }
+    public function testShow(){
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/blogentries/');
+
+//        $link = $crawler->selectLink("show")->link();
+        $link = $crawler->filter('a:contains("show")')->eq(0)->link();
+
+        $crawler = $client->click($link);
+
+        $this->assertEquals(3, $crawler->filter('th')->count());
+    }
+
+    public function testHeaderData(){
+
+        $client = static::createClient();
+
+        $client->request('GET', '/blogentries/');
+
+        $this->assertSelectorTextContains('html h1', 'Welcome to our Blog');
+
+        $this->assertSelectorTextContains('html h2', 'Blogentries index');
+    }
+
+
+    public function testEditForm(){
+
+       $client = static::createClient();
+
+       $form=$crawler = $client->request('GET', '/blogentries/new')->selectButton('Save')->form();
+
+       $form['blogentries[blog_entry]'] = 'symfony Test';
+       $form['blogentries[author]']='Jayanthi';
+
+        $crawler = $client->submit($form);
+
+//       $this->assertEquals(200, $client->getResponse()->getStatusCode());
+//        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertEquals('/blogentries/', $client->getRequest()->getPathInfo());
+        $this->assertSelectorTextContains('html h2', 'Blogentries index');
+
+   }
 }
